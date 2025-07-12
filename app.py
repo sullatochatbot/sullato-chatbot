fc10ad5654a9d6392912c6b8809fdd86e3c789c5
Anderson R. Sullato <andersonsullato@gmail.com>
2025-07-12 13:56:37 -0300

    Substituindo app.py com estrutura de verificação e token permanente

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+VERIFY_TOKEN = "sullatotoken123"
+TOKEN_META = "EAAxfFUMZAvBQBPNDsJ2obmCUPcVkOePuSpLGRP2JtAhhgPxjWHA7digp2kiDMsPiEFrgMdkOufOZBaTQHFryNZBU44WrUjhiaK53DPPcuX3WqlpSIxPJyPIinmhIyIFbZA2Nm2Hhvs3YFKstBEoakMZCnNhP8bgpKDn2x9iZApOYIYdRZBVM00IB33qjJg1zAZDZD"
+
 @app.route("/", methods=["GET"])
 def home():
     return "Chatbot Sullato online com Flask!"
@@ -11,24 +14,14 @@ def webhook():
     if request.method == "GET":
         token = request.args.get("hub.verify_token")
         challenge = request.args.get("hub.challenge")
-        if token == "sullatotoken123":
+        if token == VERIFY_TOKEN:
             return challenge
         return "Token inválido", 403
 
-    if request.method == "POST":
+    elif request.method == "POST":
         data = request.get_json()
-        print("📥 RECEBIDO DA META:")
-        print("➡️ JSON recebido:", data)
-
-        try:
-            entry = data['entry'][0]
-            message = entry['changes'][0]['value']['messages'][0]['text']['body']
-            sender = entry['changes'][0]['value']['messages'][0]['from']
-            print(f"✉️ Nova mensagem de {sender}: {message}")
-        except Exception as e:
-            print("⚠️ Erro ao interpretar mensagem:", e)
-
-        return jsonify({"status": "mensagem recebida"}), 200
+        print("Mensagem recebida:", data)
+        return "Evento recebido", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
